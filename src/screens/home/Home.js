e84b3cdc47cca0355fc680631e52bb40e06bfcc8
aftabrehan <aftabrehan52422@gmail.com>
2023-05-09 22:59:04 +0500

    Add Products Showcases (#17)

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,7 +1,7 @@
 import Header from 'components/header'
 import Hero from 'components/hero'
 import Footer from 'components/footer'
-import Showcase from 'components/showcase'
+import Feature from 'components/feature'
 
 import stl from './Home.module.scss'
 
@@ -9,7 +9,7 @@ const Home = () => (
   <div className={stl.wrapper}>
     <Header />
     <Hero />
-    <Showcase />
+    <Feature />
     <Footer />
   </div>
 )
