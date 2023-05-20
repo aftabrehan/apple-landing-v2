aa2cf42a0a02a90b32c56de1f540f1a624dfb472
aftabrehan <aftabrehan52422@gmail.com>
2023-05-20 13:06:01 +0500

    Made Header Responsive (#19)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import Link from 'next/link'
+import clsx from 'clsx'
 
 import Logo from 'assets/svg/logo.svg'
 import SearchIcon from 'assets/svg/search.svg'
 import CartIcon from 'assets/svg/cart.svg'
+import HamburgerIcon from 'assets/svg/hamburger.svg'
+import CrossIcon from 'assets/svg/cross.svg'
 
 import stl from './Header.module.scss'
 
 const Header = () => {
+  const [isHeaderExpanded, setIsHeaderExpended] = useState(false)
+
   const links = [
     'Store',
     'Mac',
@@ -20,15 +26,27 @@ const Header = () => {
   ]
 
   return (
-    <header className={stl.header}>
+    <header className={clsx(stl.header, isHeaderExpanded && stl.expanded)}>
       <Logo />
-      {links.map(link => (
-        <Link key={link} href="/">
-          {link}
-        </Link>
-      ))}
-      <SearchIcon />
-      <CartIcon />
+
+      <div className={stl.links}>
+        {links.map(link => (
+          <Link key={link} href="/">
+            {link}
+          </Link>
+        ))}
+      </div>
+
+      <div className={stl.icons}>
+        <SearchIcon />
+        <CartIcon />
+        <button
+          onClick={() => setIsHeaderExpended(!isHeaderExpanded)}
+          className={stl.hamburgerIcon}
+        >
+          {isHeaderExpanded ? <CrossIcon /> : <HamburgerIcon />}
+        </button>
+      </div>
     </header>
   )
 }
